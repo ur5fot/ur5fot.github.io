e539c7c612f520ac2bf8d5b62af2227468119347
ur5fot <wagvpered@gmail.com>
2019-07-16 18:51:53 +0300

    implement element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,36 @@
 <template>
-  <div id="app">
-   <!-- <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>-->
-    <router-view/>
-  </div>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <div>
+            <p>
+                If Element is successfully added to this project, you'll see an
+                <code v-text="'<el-button>'"></code>
+                below
+            </p>
+            <el-button>el-button</el-button>
+        </div>
+        <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+
+        <div class="block">
+            <span class="demonstration">Default</span>
+            <el-date-picker
+                    v-model="value1"
+                    type="date"
+                    placeholder="Pick a day">
+            </el-date-picker>
+        </div>
+        <div class="block">
+            <span class="demonstration">Picker with quick options</span>
+            <el-date-picker
+                    v-model="value2"
+                    type="date"
+                    placeholder="Pick a day"
+                    :picker-options="pickerOptions">
+            </el-date-picker>
+        </div>
+    </div>
+
+
 </template>
 
 <style lang="scss">
