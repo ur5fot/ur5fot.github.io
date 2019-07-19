8fa14d70644b0b4ed86096db62162c23f6647099
ur5fot <wagvpered@gmail.com>
2019-07-19 20:26:14 +0300

    feat()add @quasar/typescript ui

--- src/plugins/element.ts
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-import ElementUI from 'element-ui';
-import './element-variables.scss'
-import locale from 'element-ui/lib/locale/lang/en';
-
-
-
-Vue.use(ElementUI, {locale, size: 'small'});
